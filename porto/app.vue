ab40fdd252970f19c6fafea2be34a7f7d799a37e
alfian <alfianwilfredohoris@gmail.com>
2023-01-25 16:18:49 +0700

    add google font

--- porto/app.vue
+++ porto/app.vue
@@ -1,28 +1,45 @@
 <script>
+import iren from "./assets/iren.png";
 import Home from "./views/Home.vue";
 export default {
   name: "App",
-  components: { Home },
+  components: { Home, iren },
 };
 </script>
 <template>
-  <div class="bg-[#795548] h-screen container mx-auto px-4 grid grid-rows-10">
-    <div class="row-span-1">
-      <Home />
-    </div>
-    <div class="row-start-2">
-      <div class="grid grid-cols-12">
-        <div class="text-[100px] text-white col-start-2 col-end-7">
-          Romiwita <br />
-          Irene Klarita
+  <div
+    class="static bg-[#795548] h-screen container mx-auto px-4 grid grid-rows-6"
+  >
+    <div
+      class="bg-[url('~/assets/iren.png')] row-span-6 grid grid-rows-6 bg-no-repeat bg-center bg-contain"
+    >
+      <div class="row-span-1">
+        <Home />
+      </div>
+      <div class="row-start-2">
+        <div class="grid grid-cols-12">
+          <div class="text-[130px] text-white col-start-2 col-end-8 poirto">
+            Romiwita <br />
+            Irene Klarita
+          </div>
         </div>
       </div>
-    </div>
-    <div class="text-white grid grid-cols-12 text-right px-[10px]">
-      <div class="col-start-9 col-span-4">
-        A woman who enjoy learning, loves challenges and want to keep creating a
-        great things.
+
+      <div
+        class="text-white grid grid-cols-12 row-start-6 text-[20px] text-right px-[10px]"
+      >
+        <div class="col-start-9 col-span-4">
+          A woman who enjoy learning, loves challenges and want to keep creating
+          a great things.
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poiret+One&display=swap");
+.poirto {
+  font-family: "Poiret One", cursive;
+}
+</style>
