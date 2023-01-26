f4d0b6738b42a1f3e2c12589027945fce5a9128e
alfian <alfianwilfredohoris@gmail.com>
2023-01-26 16:27:18 +0700

    add back and fun fact in more page

--- porto/pages/about/more.vue
+++ porto/pages/about/more.vue
@@ -1,4 +1,39 @@
 <script>
-export default { name: "more" };
+import Navbar from "~~/components/Navbar.vue";
+import left from "@/components/global/left.svg";
+export default {
+  name: "more",
+  components: { Navbar, left },
+  data() {
+    return { from: "about" };
+  },
+};
 </script>
-<template><div>dari more</div></template>
+<template>
+  <div class="h-screen">
+    <div>
+      <Navbar :from="from" />
+    </div>
+    <div class="grid grid-cols-12">
+      <div class="grid content-center">
+        <div class="flex justify-center">
+          <div>
+            <!-- <nuxt-icon name="right" filled /> -->
+            <button @click="next">
+              <left class="w-7 h-10" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="poirto text-[100px]">
+          Fun <br />
+          Fact <br />
+          About <br />
+          Me
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
