6c409e664bc1f61657127ee45d6ff90a7bee4315
alfian <alfianwilfredohoris@gmail.com>
2023-01-30 10:48:39 +0700

    add method

--- porto/components/Navbar.vue
+++ porto/components/Navbar.vue
@@ -3,7 +3,21 @@ export default {
   name: "Navbar",
   props: ["from"],
   created() {
-    console.log(this.from);
+    console.log(this.from, "<");
+  },
+  methods: {
+    resume() {
+      if (this.dd === false) {
+        this.dd = true;
+      } else {
+        this.dd = false;
+      }
+    },
+  },
+  data() {
+    return {
+      dd: false,
+    };
   },
 };
 </script>
@@ -16,13 +30,18 @@ export default {
     <div class="grid content-center"><button>Home</button></div>
     <div class="grid content-center"><button>About</button></div>
     <div class="grid content-center"><button>Work</button></div>
-    <div class="grid content-center"><button>Resume</button></div>
+    <div class="grid content-center">
+      <button>Resume</button>
+    </div>
   </div>
 
   <div v-else class="h-[50px] flex flex-row text-white gap-[30px] ml-[40px]">
     <div class="grid content-center"><button>Home</button></div>
     <div class="grid content-center"><button>About</button></div>
     <div class="grid content-center"><button>Work</button></div>
-    <div class="grid content-center"><button>Resume</button></div>
+    <div class="grid content-center" @click="resume('ss')">
+      <button>Resume</button>
+      <div v-if="this.dd === true">masuk</div>
+    </div>
   </div>
 </template>
